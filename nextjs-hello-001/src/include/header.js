76b75b67bfbdc6471023ae25217ddd80f1368266
neck091 <cjstk_05@naver.com>
2024-02-28 15:11:54 +0900

    갱신

---
+++ nextjs-hello-001/src/include/header.js
@@ -0,0 +1,27 @@
+import styles from "../css/header.module.css";
+//Link를 자동 import 할 때 { Link } 처럼 import 되지 않도록 주의
+import Link from "next/link";
+const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>나의 홈페이지</h1>
+        <input></input>
+      </header>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/notice">공지사항</Link>
+          </li>
+          <li>
+            <Link href="/bbs">자유게시판</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+export default Header;
