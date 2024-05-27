af269016bc6b92c387cd7715c5f9bb6ffa9c6143
neck091 <cjstk_05@naver.com>
2024-05-27 11:16:22 +0900

    수정

--- spellfix-new/src/app/comps/SpellText.js
+++ spellfix-new/src/app/comps/SpellText.js
@@ -1,11 +1,12 @@
-"client";
 import React from "react";
 
 const SpellText = ({ text, setText }) => {
   const handleChange = (e) => {
     const newText = e.target.value;
-    setText(newText);
-    console.log("Text changed to:", newText); // 변경된 텍스트를 출력합니다.
+    if (newText.length <= 300) {
+      setText(newText);
+      console.log("변경된 텍스트:", newText); // 변경된 텍스트를 출력합니다.
+    }
   };
 
   return (
@@ -15,18 +16,16 @@ const SpellText = ({ text, setText }) => {
         <textarea
           id="inputText"
           name="text"
-          rows="5"
+          rows="13"
           value={text} // 수정된 부분: value 속성을 text 상태로 설정
           onChange={handleChange}
         ></textarea>
         <p>
           현재 입력한 글자 수:{" "}
-          <span id="currentChar">{text.length}</span> / 최대 글자 수:{" "}
+          <span id="currentChar">{text.length}</span> / 최대 글자 수:
+          {" 300"}
           <span id="maxChar"></span>
         </p>
-        <button id="showResultBtn" type="submit">
-          결과 보기
-        </button>
         <button className="delete">전부 지우기</button>
       </form>
     </div>
